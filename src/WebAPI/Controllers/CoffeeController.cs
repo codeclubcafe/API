29d73700489fb10097e40bfabae4bfe24d86c42f
Sajid Rashid <Sajid.Rashid@live.co.uk>
2018-03-23 00:12:38 +0000

    Added Coffee controller & Models etc...

---
+++ src/WebAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Coffee")]
+    public class CoffeeController : Controller
+    {
+        private Coffee _coffee;
+
+
+        public CoffeeController(Coffee coffee)
+        {
+            _coffee = coffee  ;
+        }
+        // GET: api/Coffee
+        [HttpGet]
+        public List<Coffee> Get()
+        {
+            return _coffee.GetAllCoffees();
+        }
+
+        
+       
+        
+       
+        
+        
+    }
+}
---
+++ src/WebAPI/Models/Coffee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Coffee
+    {
+        public string Name { get; set;}
+        public int Price { get; set; }
+        public string Size { get; set;}
+
+        public List<Coffee> GetAllCoffees()
+        {
+            var coffeelist = new List<Coffee>
+            {
+                new Coffee{ Name="Mocho", Price=2, Size="L"  },
+                new Coffee{ Name="Latte", Price=3, Size="XL"  },
+                new Coffee{ Name="Americano", Price=2, Size="L"  },
+                new Coffee{ Name="Expresso", Price=1, Size="XS"  }
+
+            };
+           
+            return coffeelist;
+        }
+    }
+
+
+}
